9fe1505bb358a41fa1f0cacd3a6fa7802087bd73
Hideshichan <hideshi.chan@outlook.com>
2024-08-27 12:35:37 +0100

    :pray:

--- Hideout/Features/LeapHelper.js
+++ Hideout/Features/LeapHelper.js
@@ -1,10 +1,4 @@
-register("renderEntity", (entity) => {
-    if (Player && entity.getEntity() instanceof net.minecraft.entity.player.EntityPlayer) {
-        Tessellator.pushMatrix()
-
-       /* if (Settings().ScaleOnEveryone) Tessellator.scale(Settings().CustomPlayerScale, Settings().CustomPlayerScale, Settings().CustomPlayerScale)
-
-        else */if (entity.getName() == Player.getName()) Tessellator.scale(0.3, 0.3, 0.3)
-
-    }
+register("renderEntity", (Entity, pos, ticks, event) => {
+    if (Entity.getClassName() !== "EntityPlayer" && Player.getName() !== Entity.getName()) return;
+    cancel(event)
 })
