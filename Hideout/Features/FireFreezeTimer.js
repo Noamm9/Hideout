a4bc8e340c0681800cd7a0b42584bad8ce5ae988
Noamm <116365534+Noamm9@users.noreply.github.com>
2024-09-23 00:51:18 +0300

    Update FireFreezeTimer.js
    your choice to accept it tbh i dont even know if its better

--- Hideout/Features/FireFreezeTimer.js
+++ Hideout/Features/FireFreezeTimer.js
@@ -1,29 +1,26 @@
 import config from "../data/config"
-import {
-    RED,
-    AQUA
-} from "../utils/stuff"
+import { RED, AQUA, GREEN, YELLOW } from "../utils/stuff"
+import { registerWhen } from "../../BloomCore/utils/Utils"
 
-register("chat", () =>{
-    if (!config().FireFreezeToggle) return;
-    new Thread(() => {
-        Thread.sleep(200);
-        World.playSound("note.pling", 2, 1);
-        Client.showTitle(`${RED}Use FFS in 5...`, "", 0, 21, 0);
-        Thread.sleep(1000);
-        World.playSound("note.pling", 2, 1);
-        Client.showTitle(`${RED}Use FFS in 4...`, "", 0, 21, 0);
-        Thread.sleep(1000);
-        World.playSound("note.pling", 2, 1);
-        Client.showTitle(`${RED}Use FFS in 3...`, "", 0, 21, 0);
-        Thread.sleep(1000);
-        World.playSound("note.pling", 2, 1);
-        Client.showTitle(`${RED}Use FFS in 2...`, "", 0, 21, 0);
-        Thread.sleep(1000);
-        World.playSound("note.pling", 2, 1);
-        Client.showTitle(`${RED}Use FFS in 1...`, "", 0, 21, 0);
-        Thread.sleep(1000);
-        World.playSound("random.anvil_land", 2, 1);
-        Client.showTitle(`${AQUA}Use FFS NOW`, "", 0, 21, 0);
-    }).start();
-}).setCriteria("[BOSS] The Professor: Oh? You found my Guardians' one weakness?")
+const TimerAction = new Thread(() => {
+    Thread.sleep(200);
+    World.playSound("note.pling", 2, 1);
+    Client.showTitle(`${RED}Use FFS in 5...`, "", 0, 21, 0);
+    Thread.sleep(1000);
+    World.playSound("note.pling", 2, 1);
+    Client.showTitle(`${RED}Use FFS in 4...`, "", 0, 21, 0);
+    Thread.sleep(1000);
+    World.playSound("note.pling", 2, 1);
+    Client.showTitle(`${RED}Use FFS in 3...`, "", 0, 21, 0);
+    Thread.sleep(1000);
+    World.playSound("note.pling", 2, 1);
+    Client.showTitle(`${RED}Use FFS in 2...`, "", 0, 21, 0);
+    Thread.sleep(1000);
+    World.playSound("note.pling", 2, 1);
+    Client.showTitle(`${RED}Use FFS in 1...`, "", 0, 21, 0);
+    Thread.sleep(1000);
+    World.playSound("random.anvil_land", 2, 1);
+    Client.showTitle(`${AQUA}Use FFS NOW`, "", 0, 21, 0);
+})
+
+registerWhen(register("chat", () => TimerAction.start()).setCriteria("[BOSS] The Professor: Oh? You found my Guardians' one weakness?"), () => config().FireFreezeToggle)
