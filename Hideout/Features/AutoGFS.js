aba3603296bf09ef7651a0e31209657caa5b22e4
Hideshichan <hideshi.chan@outlook.com>
2024-09-23 20:10:07 +0100

    again quick fix

--- Hideout/Features/AutoGFS.js
+++ Hideout/Features/AutoGFS.js
@@ -58,6 +58,6 @@ function Jerry() {
 register("command", Jerry).setCommandName("ij").setAliases(["ijerry", "ijerrys", "jerry", "jerrys", "jerries", "ijerries"])
 
 registerWhen(register("chat", Pearls).setCriteria("Starting in 3 seconds."), () => config().AutoGFS && config().GFSPearls)
-registerWhen(register("chat", Jerry).setCriteria("Starting in 2 seconds."), () => config().AutoGFS && config().GFSPearls)
+registerWhen(register("chat", Jerry).setCriteria("Starting in 2 seconds."), () => config().AutoGFS && config().GFSJerry)
 
 registerWhen(register("chat", event => cancel(event)).setCriteria(/Moved .+ (Ender Pearl|Inflatable Jerry) from your Sacks to your inventory\./), () => config().AutoGFS && (config().GFSPearls || config().GFSJerry))
