87d20d65afbb591c910992f15806e3d468358cf7
Noamm <116365534+Noamm9@users.noreply.github.com>
2024-09-26 09:14:47 +0300

    optimization

--- Hideout/Features/TerminalWaypoints.js
+++ Hideout/Features/TerminalWaypoints.js
@@ -1,78 +1,12 @@
-/*import config from "../data/config"
-import renderBeaconBeam from "../../BeaconBeam"
-import { GetP3Section, drawTrace } from "../utils/stuff"
-
-let in_p3 = false
-
-// Guess who made this code (hint: starts with a N and has worked too hard to help me <3)
-
-export const Terminal_Locations = [
-    [ // S1
-        [111, 113, 73],
-        [111, 119, 79],
-        [89, 112, 92],
-        [89, 122, 101],
-    ],
-
-    [ // S2
-        [68, 109, 121] ,
-        [59, 120, 121] ,
-        [47, 109, 121] ,
-        [39, 108, 138] ,
-        [40, 124, 122] ,
-    ],
-
-    [ // S3
-        [-3, 109, 112] ,
-        [-3, 109, 93],
-        [20, 123, 93],
-        [-3, 109, 77],
-    ],
-
-    [ // S4
-        [41, 109, 29],
-        [44, 121, 29],
-        [67, 109, 29],
-        [72, 115, 47] 
-    ]
-]
-
-
-register("chat", (event) => in_p3 = true).setCriteria("[BOSS] Storm: I should have known that I stood no chance.")
-register("chat", event => in_p3 = false).setCriteria("The Core entrance is opening!")
-
-function DoStuff(term, color) {
-    const sectiontoload = Terminal_Locations[GetP3Section() - 1]
-    renderBeaconBeam(
-        //...sectiontoload[term], 
-        sectiontoload[term][0],sectiontoload[term][1],sectiontoload[term][2],
-        color[0], color[1], color[2],
-        0.8, false, 100
-    )
-    if (!config().TermTracers) return;
-    drawTrace(
-        //...sectiontoload[term], if i figure out how to fix this ill use it bc it looks wayyyy fucking better
-        sectiontoload[term][0],sectiontoload[term][1],sectiontoload[term][2],
-        color[0], color[1], color[2],
-    )
-}
-
-register("renderWorld", () =>{
-    if (!config().TermWaypoints) return;
-    if (!in_p3) return;
-
-    DoStuff(config().WhichTerm, config().TermWaypointsColor)
-})*/
-
 import config from "../data/config"
 import renderBeaconBeam from "../../BeaconBeam"
-import { GetP3Section, drawTrace } from "../utils/stuff"
-
-let in_p3 = false
+import { GetP3Section, drawTrace, getPhase } from "../utils/stuff"
+import { registerWhen } from "../../BloomCore/utils/Utils"
 
 // Guess who made this code (hint: starts with a N and has worked too hard to help me <3)
+// @Noamm9 me!
 
-export const Terminal_Locations = [
+const Terminal_Locations = [
     [ // S1
         [111, 113, 73],
         [111, 119, 79],
@@ -103,13 +37,10 @@ export const Terminal_Locations = [
     ]
 ]
 
-
-register("chat", () => in_p3 = true).setCriteria("[BOSS] Storm: I should have known that I stood no chance.")
-register("chat", () => in_p3 = false).setCriteria("The Core entrance is opening!")
 
 function DoStuff(term, color) {
     Terminal_Locations.forEach((value, index) => {
-        if (index != GetP3Section - 1) return
+        if (index != GetP3Section() - 1) return
 
         value.forEach((termCoords, termNumber) => {
             if (termNumber == term) {
@@ -130,9 +61,6 @@ function DoStuff(term, color) {
     })
 }
 
-register("renderWorld", () =>{
-    if (!config().TermWaypoints) return;
-    if (!in_p3) return;
-
+registerWhen(register("renderWorld", () => 
     DoStuff(config().WhichTerm, config().TermWaypointsColor)
-})
+), () => config().TermWaypoints && getPhase() == "p3")
